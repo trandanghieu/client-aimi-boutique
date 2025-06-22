82450e62664419ce6db24c552a6338d16d2b16a4
Hieu Tran <trandanghieu2432002@gmail.com>
2025-06-22 17:43:25 +0700

    setup: Initial Project Vite & Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff8901",
+        secondary: "#fb923c",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
